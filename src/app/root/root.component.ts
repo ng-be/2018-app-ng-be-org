1617948f25d11286d06bf055d29413b51e2aeab1
Sam Verschueren <sam.verschueren@gmail.com>
2018-12-06 18:53:10 +0100

    Remove messaging

--- src/app/root/root.component.ts
+++ src/app/root/root.component.ts
@@ -1,13 +1,7 @@
 import {Component} from '@angular/core';
-import * as firebase from 'firebase';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: 'root.component.html'
 })
-export class RootComponent {
-	async ngOnInit() {
-		const messaging = firebase.messaging();
-		await messaging.requestPermission();
-	}
-}
+export class RootComponent {}
