6c1f67b1a388aea59c163b94cb87da66c04483c1
Sam Verschueren <sam.verschueren@gmail.com>
2018-12-06 15:46:08 +0100

    Add firebase deployment process

--- src/app/root/root.component.ts
+++ src/app/root/root.component.ts
@@ -1,7 +1,13 @@
 import {Component} from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: 'root.component.html'
 })
-export class RootComponent {}
+export class RootComponent {
+	async ngOnInit() {
+		const messaging = firebase.messaging();
+		await messaging.requestPermission();
+	}
+}
